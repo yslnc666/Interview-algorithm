dfde87fb0afb68d4e54d66aa1b8c500209b9253d
dellG3 <2501827647@qq.com>
2025-03-14 11:44:51 +0800

    add ctrip0313

---
+++ src/ctrip0313/ConnectFirstString.java
@@ -0,0 +1,21 @@
+package ctrip0313;
+
+import java.util.Scanner;
+
+public class ConnectFirstString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        int len = s.length();
+        int index = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; index < len; i++) {
+            index += i;
+            if (index < len) {
+                sb.append(s.charAt(index));
+            }
+        }
+        System.out.println(sb.toString());
+        in.close();
+    }
+}
